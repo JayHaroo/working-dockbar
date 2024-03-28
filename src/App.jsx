6f3ed74944a7d0a6b3c0d0902d1da4d6ca1504a6
JeyHaroo <mjeyict3@gmail.com>
2024-03-28 10:48:05 +0800

    Changed color palette

--- src/App.jsx
+++ src/App.jsx
@@ -16,41 +16,51 @@ function App() {
 
   return (
     <>
-      <React.Fragment>
-        <div
-          className="text-5xl font-bold cursor-pointer ml-2 mt-2"
-          onClick={openDrawer}
-        >
-          {" "}
-          ☰
-        </div>
-        <Drawer open={open} onClose={closeDrawer} className="p-4 bg-black flex-col flex items-center">
-          <div className="flex align-middle">
-            <img src={logo} alt="logo" className="w-3/4" />
-            <IconButton variant="text" color="blue-gray" onClick={closeDrawer}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={2}
-                stroke="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </IconButton>
+      <body className="min-h-screen bg-[#1A120B]">
+        <React.Fragment>
+          <div
+            className="text-5xl text-[#D5CEA3] font-bold cursor-pointer pt-2 pl-2"
+            onClick={openDrawer}
+          >
+            {" "}
+            ☰
           </div>
-          <ul className="text-white cursor-pointer mt-3">
-            <li>Products</li>
-            <li>Our Services</li>
-            <li>FAQ</li>
-          </ul>
-        </Drawer>
-      </React.Fragment>
+          <Drawer
+            open={open}
+            onClose={closeDrawer}
+            className="p-4 bg-[#3C2A21] flex-col flex items-center"
+          >
+            <div className="flex align-middle">
+              <img src={logo} alt="logo" className="w-3/4" />
+              <IconButton
+                variant="text"
+                color="blue-gray"
+                onClick={closeDrawer}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </IconButton>
+            </div>
+            <ul className="text-white cursor-pointer mt-3">
+              <li>Products</li>
+              <li>Our Services</li>
+              <li>FAQ</li>
+            </ul>
+          </Drawer>
+        </React.Fragment>
+      </body>
     </>
   );
 }
